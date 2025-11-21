848f83b09a168eb39c1274cd5a4696b4cb22def9
Jack Yang <jackyangzzh@gmail.com>
2025-11-21 11:30:03 -0500

    Fix blog issue

--- _plugins/jekyll-display-medium-posts-json.rb
+++ _plugins/jekyll-display-medium-posts-json.rb
@@ -15,9 +15,17 @@ module Jekyll
       end
 
       feed_url = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@#{username}"
-      raw       = Net::HTTP.get(URI(feed_url))
-      json      = JSON.parse(raw)
-      items     = json["items"] || []
+      uri = URI(feed_url)
+      
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE  # Skip SSL verification for this API
+      
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
+      
+      json = JSON.parse(response.body)
+      items = json["items"] || []
 
       Jekyll.logger.info  "MediumPosts:", "got #{items.size} items"
 
